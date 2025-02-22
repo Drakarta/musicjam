2869ca6483b3b0a6243988d072984dedddd42bf7
Anthony <drakarta3@gmail.com>
2025-02-22 17:02:15 +0100

    setup tanstack router and querry

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+import { routeTree } from './routeTree.gen'
+
+const router = createRouter({ routeTree })
+const queryClient = new QueryClient()
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 )
